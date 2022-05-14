69d0df8c3537a2b88957f295aafae280b51bdf2b
mdsujon-mridha <mdsujan134523@gmail.com>
2022-05-14 14:11:56 +0600

    appointment page add

---
+++ src/pages/Components/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import docotor from '../../../assets/images/doctor.png';
+import appoinment from '../../../assets/images/appointment.png'
+const MakeAppointment = () => {
+    return (
+       <section style={{
+           background: `url(${appoinment})`
+       }}>
+           <div className="flex justify-center items-center">
+              <div className="flex-1 hidden lg:block">
+              <img src={docotor} className='mt-[-200px]' alt="" />
+              </div>
+              <div className="flex-1 px-12">
+                  <h3 className="text-xl text-primary font-bold leading-10">Appointment</h3>
+                  <h2 className="text-4xl text-white leading-10">Make an Appointment today</h2>
+                  <p className="text-white text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                  <button class="mt-3.5 btn btn-secondary text-white font-bold bg-gradient-to-r from-secondary to-primary ...">Get Started</button>
+              </div>
+           </div>
+
+       </section>
+    );
+};
+
+export default MakeAppointment;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,16 @@ import banner1 from '../../assets/images/chair.png'
 import bannerBackground from '../../assets/images/bg.png'
 import Info from './Info/Info';
 import Services from '../Components/Services/Services';
+import DentalCare from '../Components/DentalCare/DentalCare';
+import MakeAppointment from '../Components/MakeAppointment/MakeAppointment';
 const Home = () => {
     return (
 
         <div>
 
-<div class="hero min-h-screen px-12">
+<div class="hero min-h-screen px-12" style={{
+  background:`url(${bannerBackground})`
+}}>
   <div class="hero-content flex-col lg:flex-row-reverse">
     <img src={banner1} class="max-w-lg rounded-lg shadow-2xl" />
     <div>
@@ -22,6 +26,8 @@ const Home = () => {
 {/* infocard  */}
 <Info></Info>
 <Services></Services>
+<DentalCare></DentalCare>
+<MakeAppointment></MakeAppointment>
 
         </div>
 
