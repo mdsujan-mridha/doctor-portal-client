382b070b003610d981dcb9b5b2b6493595f63546
mdsujon-mridha <mdsujan134523@gmail.com>
2022-05-22 20:16:11 +0600

    appointment date and card implement

--- src/pages/Appointment/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments.js
@@ -1,24 +1,33 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
+import Schedule from './Schedule';
 
 const AvailableAppointments = ({date,setDate}) => {
     const[schedules, setSchedules] = useState([]);
+    const [treatment ,setTreatment] = useState(null);
     useEffect( ()=>{
         fetch('Schedule.json')
          .then(res => res.json())
           .then(data => setSchedules(data) )
-
+           
+        //    console.log(schedules);
     } ,[])
     return (
         <div>
             <div>
             <p className='text-secondary font-bold text-center text-xl lg: mt-[-120px]'> Available Appointments on {format(date, 'PP')}.</p>
             </div>
-            <div>
+            <div className=' px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10'>
                 {
-                    schedules.map(schedule=>console.log(schedule))
+                    schedules.map(schedule=><Schedule
+                    key={schedule._id}
+                    schedule={schedule}
+                    setTreatment={setTreatment}
+                    ></Schedule>)
                 }
             </div>
+            {treatment && <BookingModal treatment={treatment}></BookingModal>}
         </div>
     );
 };
--- src/pages/Appointment/Schedule.js
+++ src/pages/Appointment/Schedule.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const Schedule = () => {
+const Schedule = ({ schedule,setTreatment }) => {
+    // console.log(schedule);
+    const { name, time } = schedule;
+
     return (
         <div>
-            
+
+            <div class="card lg:max-w-md bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-xl font-semibold text-secondary mt-10 text-center"> {name} </h2>
+                     <p className='text-center mt-2.5'>
+                         {
+                             time.length 
+                             ? <span>{time[0]}</span>
+                             :
+                             <span className='text-red-500'> slot out </span> 
+                         }
+                     </p>
+                    <p className='text-center mt-3'> {time.length} {time.length > 1 ? "spaces" : "space"}  are available </p>
+                    <div class="card-actions justify-center mt-4">
+                        <label 
+                        for="booking-modal"
+                        disabled={time.length === 0} 
+                        onClick={()=> setTreatment(schedule)} 
+                        class="btn modal-button btn btn-secondary font-bold text-white text-sm uppercase"> Book Appointment </label>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
